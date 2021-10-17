f0bc82ab4e2e41212054ea8b5c3d57dce5839934
Thorsten <88725484+IMC-GER@users.noreply.github.com>
2021-10-17 15:36:39 +0200

    Using phpBB’s template syntax

---
+++ imcger/fancybox/styles/all/template/fancybox/README.md
@@ -0,0 +1,20 @@
+# phpBB-Fancybox
+
+## Installation
+
+Fancybox v3 is licensed under the [GPLv3](https://www.gnu.org/licenses/gpl-3.0) license for all open source applications.
+A commercial license is required for all commercial applications (including sites, themes and apps you plan to sell). [Fancybox 3](https://fancyapps.com/fancybox)
+
+If you use Fancybox v4 you need a Fancyapps license. More infos [Fancyapps](https://fancyapps.com).
+Buy a license and  copy the `fancybox.css` and `fancybox.umd.js` to `phpBB3/ext/imcger/fancybox/styles/all/fancybox`.
+
+Go to "ACP" > "Customise" > "Extensions Manager" and enable the "Fancybox" extension.
+
+## License
+**Fancybox v3**
+Is licensed under the [GPLv3](https://www.gnu.org/licenses/gpl-3.0) license for all open source applications.
+A commercial license is required for commercial use.
+
+**Fancybox v4** 
+You need a Fancybox license for none commercial use and commercial use.
+[Fancyapps](https://fancyapps.com)
---
+++ imcger/fancybox/styles/all/template/fancybox/fancybox-prepare.js
@@ -0,0 +1,58 @@
+/**
+		IMC JavaScript Programm
+	--------------------------------------------------------------
+		Name: fancybox-prepare.js
+		Last edit: 29.08.2021
+		Copyright: (c) 2021 Thorsten Ahlers
+		License: GNU General Public License, version 2 (GPL-2.0)
+	--------------------------------------------------------------	
+		AddOn für die phpBB Forum Software.
+		Das Skript durchsucht die Seite nach Dateianhängen verlinkte Bilder
+		und Bilder die mit den BBCode eingefügt sind. Den Elementen werden
+		die Fancybox Attribute hinzugefügt damit diese mit den Fancybox
+		Bildbetrachter geöffnet werden können.
+*/
+
+var x, i;
+
+/* Dateianhänge  S_THUMBNAIL */
+x = document.querySelectorAll("a > .postimage");
+for (i = 0; i < x.length; i++) {
+	x[i].closest('a').setAttribute("data-fancybox","image");
+	x[i].closest('a').setAttribute("data-caption", x[i].getAttribute("alt"));
+}
+
+/* Dateianhänge  S_IMAGE */
+x  = document.querySelectorAll(".attach-image > .postimage");
+for (i = 0; i < x.length; i++) {
+	var srcAttr = x[i].getAttribute("src");
+	var altAttr = x[i].getAttribute("alt");
+	var newEle = '<a href="' + srcAttr + '" data-fancybox="image" data-caption="' + altAttr + '">' + x[i].outerHTML + '</a>';
+
+	x[i].insertAdjacentHTML("afterend",newEle);
+	x[i].remove();
+}
+
+/* Verlinkte Bilder  BBCode IMG */
+x  = document.querySelectorAll(".content > .postimage");
+for (i = 0; i < x.length; i++) {
+	var srcAttr = x[i].getAttribute("src");
+	var newEle = '<a href="' + srcAttr + '" data-fancybox="image" data-caption="' + srcAttr + '">' + x[i].outerHTML + '</a>';
+
+	x[i].insertAdjacentHTML("afterend",newEle);
+	x[i].remove();
+}
+
+/* Verlinkte Bilder */
+x  = document.querySelectorAll(".postlink");
+for (i = 0; i < x.length; i++) {
+	var ext = ["jpg", "jpeg", "gif", "png", "webp", "bmp"];
+	var url = x[i].getAttribute("href");
+	var usa = url.split(".");
+	
+	/* Wenn auf ein erlaubtes Bild verlinkt wird Attribute setzen */
+	if(ext.includes(usa[usa.length-1].toLocaleLowerCase())) {
+		x[i].setAttribute("data-fancybox","image");
+		x[i].setAttribute("data-caption", url);
+	}
+}
